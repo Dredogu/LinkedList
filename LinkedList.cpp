64345a36f03087d8180972f690c0f2c95493231f
Dredogu <dgnk6606@gmail.com>
2022-11-05 18:11:38 +0300

    Add LinkedList.cpp 1.0

---
+++ LinkedList.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+class Node{
+public:
+    T data;
+    Node<T> *next;
+public:
+    Node(T data){
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+template<class T>
+class LinkedList{
+private:
+    Node<T> *first = nullptr;
+public:
+    void Append(T data){
+        Node<T> *node = new Node<T>(data);
+        node->next = nullptr;
+
+        if(first == nullptr){
+            first = node;
+        }
+        else{
+            Node<T> *last = first;
+            while(last->next){
+                last = last->next;
+            }
+            last->next = node;
+        }
+    }
+
+    void InsertFirst(T data){
+        Node<T> *node = new Node<T>(data);
+        node->next = nullptr;
+
+        if(first == nullptr){
+            first = node;
+        }
+        else{
+            node->next = first;
+            first = node;
+        }
+    }
+
+    void Insert(int pos, T data){
+        if(pos < 1){
+            return;
+        }
+        else if(pos == 1){
+            this->InsertFirst(data);
+        }
+        else{
+            Node<T> *node = new Node<T>(data);
+            node->next = nullptr;
+            Node<T> *p = first;
+
+            for(int i = 1; i < pos-1 && p->next; i++){
+                p = p->next;
+            }
+
+            node->next = p->next;
+            p->next = node;
+        }
+    }
+
+    void Print(){
+        Node<T> *p = first;
+        while(p){
+            cout << p->data << endl;
+            p = p->next;
+        }
+    }
+};
+
+
+int main(){
+
+    LinkedList<string> list;
+    
+    list.Append("Dre");
+    list.Append("Memo");
+    list.Append("Musti");
+    list.Append("Atakan");
+
+    list.InsertFirst("Nihat");
+
+    list.Insert(2, "Asim");
+    list.Insert(1, "Feride");
+
+    list.Print();
+
+    return 0;
+}
